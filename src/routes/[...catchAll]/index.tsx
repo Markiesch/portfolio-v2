0f17d567f793c05343d6047a75a7378231ea5a54
Mark Schuurmans <marksmeel@gmail.com>
2023-03-22 23:39:10 +0100

    Feat: added 404 error page

---
+++ src/routes/[...catchAll]/index.tsx
@@ -0,0 +1,25 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from "../../styles/404.scss?inline";
+
+export default component$(() => {
+  useStylesScoped$(styles);
+
+  return (
+    <div class="container">
+      <span>Something went wrong...</span>
+      <h1 class="section--title">Oops! We couldn't find that page.</h1>
+      <p class="text-body subtitle">Maybe you can find what you need here?</p>
+      <div class="link-container">
+        <a class="button primary" href="/">
+          Home
+        </a>
+        <a class="button" href="/portfolio">
+          Projects
+        </a>
+        <a class="button" href="/contact">
+          Contact
+        </a>
+      </div>
+    </div>
+  );
+});
