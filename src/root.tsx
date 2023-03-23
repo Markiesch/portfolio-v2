a78ec7707b0e69238657ef6bb5a257304f81820c
Markiesch <marksmeel@gmail.com>
2023-03-23 21:08:58 +0100

    Feat: added meta tags

--- src/root.tsx
+++ src/root.tsx
@@ -8,25 +8,50 @@ import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
 
-export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
-
-  return (
+export default component$(() =>
+  (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
+        <title>Mark Schuurmans - Portfolio</title>
+        <meta name="author" content="Mark Schuurmans"/>
+        <meta name="description" content="I design and develop beautiful responsive websites"/>
+        <meta name="keywords"
+              content="web, website, responsive, developer, frontend, backend, portfolio, projects, vuejs, vue, desktop, mobile, tablet"/>
+
+        <meta name="og:title" content="Portfolio - Mark Schuurmans"/>
+        <meta name="og:type" content="website"/>
+        <meta name="og:url" content="https://www.markschuurmans.nl"/>
+        <meta name="og:image" content="/assets/mockups/home-page.png"/>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        <meta charSet="utf-8"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+
+        <RouterHead/>
       </head>
       <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
+      <RouterOutlet/>
+      <ServiceWorkerRegister/>
       </body>
     </QwikCityProvider>
-  );
-});
+  ));
