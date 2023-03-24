6e8f80bee78a71e36600bcc5bf6f2171102321d4
Markiesch <marksmeel@gmail.com>
2023-03-24 22:02:35 +0100

    Feat: added gradient to hero section

--- src/components/home/hero/hero.tsx
+++ src/components/home/hero/hero.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {component$, useStylesScoped$} from "@builder.io/qwik";
 import styles from "./hero.scss?inline";
 import ArrowRightIcon from "~/components/icons/arrow-right.icon";
 import MailIcon from "~/components/icons/mail.icon";
@@ -7,21 +7,23 @@ export default component$(() => {
   useStylesScoped$(styles);
 
   return (
-    <div class="hero container">
-      <span class="subheading gradient-text">Design. Build. Improve.</span>
-      <h1 class="gradient-text">I build websites your users will love</h1>
-      <p>
-        Hi I am Mark, a Software Developer with an eye for UI design. I am
-        specialized in UI/UX design and responsive web development.
-      </p>
+    <div class="wrapper">
+      <div class="hero container">
+        <span class="subheading gradient-text">Design. Build. Improve.</span>
+        <h1 class="gradient-text">I build websites your users will love</h1>
+        <p>
+          Hi I am Mark, a Software Developer with an eye for UI design. I am
+          specialized in UI/UX design and responsive web development.
+        </p>
 
-      <div>
-        <a href="/projects" class="button primary">
-          <span>View projects</span> <ArrowRightIcon />
-        </a>
-        <a href="/contact" class="button">
-          <MailIcon /> <span>Contact</span>
-        </a>
+        <div>
+          <a href="/projects" class="button primary">
+            <span>View projects</span> <ArrowRightIcon/>
+          </a>
+          <a href="/contact" class="button">
+            <MailIcon/> <span>Contact</span>
+          </a>
+        </div>
       </div>
     </div>
   );
