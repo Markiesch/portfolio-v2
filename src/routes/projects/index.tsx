d602be7fde5f34a016c838e011c9182e79ce74cc
Markiesch <marksmeel@gmail.com>
2023-03-24 22:43:52 +0100

    Feat: added basic markup for projects page

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,5 +1,25 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from "../../styles/pages/_projects.scss?inline";
+import { projects } from "~/components/home/project-cards/projects";
+import ProjectCard from "~/components/project-card/project-card";
 
 export default component$(() => {
-  return <p>Projects</p>;
+  useStylesScoped$(styles);
+
+  return (
+    <>
+      <section class="hero">
+        <h1 class="gradient-text">Projects</h1>
+        <p>Here you will find a few projects I made in my free time, or while learning a new technology.</p>
+      </section>
+      <section class="project-section container">
+        {
+          projects.map(project => (
+            <ProjectCard key={project.id} project={project} />
+          ))
+
+        }
+      </section>
+    </>
+  );
 });
