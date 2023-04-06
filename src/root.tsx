baf63df9cd56e5738319fc1e67ad337ee87e3221
Markiesch <marksmeel@gmail.com>
2023-04-06 22:04:09 +0200

    Fix: build error

--- src/root.tsx
+++ src/root.tsx
@@ -50,12 +50,12 @@ export default component$(() => (
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
       <link
         rel="preload"
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-        onload="this.onload=null;this.rel='stylesheet'"
+        onLoad="this.onload=null;this.rel='stylesheet'"
         as="style"
       />
 
